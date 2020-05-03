71b3233f45ac25b00be1b9d0d7c8845208e71289
HughG-50 <hugh.greethead@gmail.com>
2020-05-04 09:42:39 +1000

    Pages controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # putting link to home page anywhere in app we can refer to it as root_path
+  get "/", to: "pages#home", as: "root"
 end
