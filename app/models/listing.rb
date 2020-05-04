aaff81109f526789b66eb088eb7f3508a470a74d
HughG-50 <hugh.greethead@gmail.com>
2020-05-04 10:31:56 +1000

    Listing entity relationships

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,6 @@
 class Listing < ApplicationRecord
   belongs_to :user
+  # all of these need to be there in order to create a new listing
+  validates :title, :description, :price, presence: true
+  has_one_attached :picture
 end
