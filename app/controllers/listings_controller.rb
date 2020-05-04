4510ab357c2b5c2ffc59ead3983f9dac18ebe8e0
HughG-50 <hugh.greethead@gmail.com>
2020-05-04 14:12:47 +1000

    Edit and update

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -6,9 +6,67 @@ class ListingsController < ApplicationController
         @listings = Listings.all
     end
 
+    # THIS IS JUST A GET REQUEST -RENDERING HTML FORM
     def new 
         # making a new instance of the Listing object (it is blank)
         # we use this within a Form 
         @listing = Listing.new
     end
+
+    def show 
+        @listing = Listing.find(params["id"])
+    end
+
+    def create 
+        @listing = current_user.listings.create(listing_params)
+        if @listing.errors.any?
+            render "new"
+        else 
+            redirect_to listings_path 
+        end
+
+        # redirects back to where we are showing all of the listings
+        redirect_to listings_path
+    end
+
+    # We ONLY want User who created the listing able to edit it
+    # ALL THIS IS DOING IS RENDERING HTML FOR THE FORM - GET REQUEST
+    # Instead of looking through all of the listings in the app for the id, we will
+    # only look through the listings of the current user
+    def edit 
+        @listing = current_user.listings.find_by_id(params["id"])
+
+        # if we have found a listing render the edit page, otherwise redirect back to main listings page
+        if @listing 
+            render("edit")
+        else 
+            redirect_to listings_path
+        end
+    end
+
+    # UPDATES THE ACTUAL DATABASE (PUT AND PATCH REQUESTS)
+    def update 
+        @listing = current_user.listings.find_by_id(params["id"])
+        
+        if @listing
+            @listing.update(listing_params)
+            if @listing.errors.any?
+                render "edit"
+            else
+                redirect_to listings_path
+            end
+        else 
+            redirect_to listings_path
+        end
+    end
+
+
+    # code inside object only can access this code
+    private 
+
+    # very important to utilize this
+    def listing_params
+        params.require(:listing).permit(:title, :price, :description, :picture)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   # putting link to home page anywhere in app we can refer to it as root_path
   get "/", to: "pages#home", as: "root"
-
   get "/listings", to: "listings#index", as: "listings"
-
   get "listings/new", to: "listings#new", as: "new_listing"
+  post "/listings", to: "listings#create"
+  # dynamic routes
+  get "listings/:id", to: "listings#show", as: "listing"
+  get "/listings/:id/edit", to: "listings#edit", as: "edit_listing"
+  put "/listings/:id", to: "listings#update"
+  patch "/listings/:id", to: "listings#update"
 end
