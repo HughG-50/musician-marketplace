6bf4454288918ab8d76c26dc91601e1276faa7ba
HughG-50 <hugh.greethead@gmail.com>
2020-05-04 10:06:55 +1000

    Added Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # shorthand for all of the routes that devise needs in order for our application to work
+  devise_for :users
   # putting link to home page anywhere in app we can refer to it as root_path
   get "/", to: "pages#home", as: "root"
 end
